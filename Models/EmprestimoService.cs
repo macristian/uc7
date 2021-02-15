507d5f3cf8239ecf0ec6655c8730184cab565e06
Cristian Bernardo <cristian.silva@ceca.ufal.br>
2021-02-14 23:33:56 -0300

    Correção de Adicionar Registro de Devolução do Livro

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -26,6 +26,7 @@ namespace Biblioteca.Models
                 emprestimo.LivroId = e.LivroId;
                 emprestimo.DataEmprestimo = e.DataEmprestimo;
                 emprestimo.DataDevolucao = e.DataDevolucao;
+                emprestimo.Devolvido = e.Devolvido;
 
                 bc.SaveChanges();
             }
